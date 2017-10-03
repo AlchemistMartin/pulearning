6934fd6dd71004f16029f3de10b4ce5a41835ba4
Martin <martin19920@gmail.com>
2017-10-03 10:35:05 +0800

    qiucker

--- putree.py
+++ putree.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import multiprocessing as mp
+import pandas as pd
+import sys
+import time
 from scipy.stats import rankdata
 
 
@@ -10,35 +14,40 @@ class PUTREE():
 		self.min_size = min_size
 		self.tree = {}
 
-	# Select the best split point for a dataset
-	def get_split(self, dataset, lables):
-		unl = max(abs(np.sum((lables-1)/2)), 1e-8)
-		pos = max(abs(np.sum((lables+1)/2)), 1e-8)
+		# Select the best split point for a dataset
+	def get_split(self, dataset, lables, feature_id):
+		unl_cnt = max(abs(np.sum((lables-1)/2)), 1e-8)
+		pos_cnt = max(abs(np.sum((lables+1)/2)), 1e-8)
+		total_cnt = unl_cnt+pos_cnt
+		pos = (lables+1)/2
+		unl = -(lables-1)/2
+		p_factor = 1.0*(unl_cnt/pos_cnt)*self.pos_level
 		sp_index, sp_value, sp_gini = 999, -1e08, 1e08
-		for index in range(0, dataset.shape[1]):
+		for index in feature_id:
 			data_sp = dataset[:, index]
-			values = np.unique(data_sp)
-			for value in values:
-				left = np.where(data_sp <= value)[0]
-				right = np.where(data_sp > value)[0]
-				l_p = max(np.sum((lables[left]+1)/2), 1e-8)
-				l_n = max(-np.sum((lables[left]-1)/2), 1e-8)
-				r_p = max(np.sum((lables[right]+1)/2), 1e-8)
-				r_n = max(-np.sum((lables[right]-1)/2), 1e-8)
-				l = left.shape[0]*1.0
-				r = right.shape[0]*1.0
-				l_p1 = min(1, (l_p/pos)*self.pos_level*(unl/l_n))
-				r_p1 = min(1, (r_p/pos)*self.pos_level*(unl/r_n))
-				l_gini = 1-(l_p1*l_p1+(1-l_p1)*(1-l_p1))
-				r_gini = 1-(r_p1*r_p1+(1-r_p1)*(1-r_p1))
-				gini_node = l/(l+r)*l_gini+r/(l+r)*r_gini
-				print(index, value, gini_node)
-				if gini_node < sp_gini:
-					sp_index = index
-					sp_value = value
-					sp_gini = gini_node
-					sp_groups = (dataset[left], dataset[right])
-					sp_lables = (lables[left], lables[right])
+			data_df = pd.DataFrame({'data': data_sp, 'pos': pos, 'unl': unl, 'lable': lables})
+			f = {'pos': 'sum', 'unl': 'sum', 'lable': 'count'}
+			l_gini_df = data_df.groupby('data').agg(f).sort_index(ascending=True).cumsum()
+			r_gini_df = data_df.groupby('data').agg(f).sort_index(ascending=False).cumsum()
+			gini_df = l_gini_df.join(r_gini_df, how='left', lsuffix='_l', rsuffix='_r')
+			gini_df['lable_r'] = total_cnt-gini_df['lable_l']
+			del data_df, l_gini_df, r_gini_df
+			gini_df['p1_l'] = 1.0*gini_df['pos_l']/gini_df['unl_l']*p_factor
+			gini_df['p1_r'] = 1.0*gini_df['pos_r']/gini_df['unl_r']*p_factor
+			gini_df.loc[gini_df['p1_l'] > 1, 'p1_l'] = 1
+			gini_df.loc[gini_df['p1_r'] > 1, 'p1_r'] = 1
+			gini_df['gini_l'] = 1-(gini_df['p1_l']*gini_df['p1_l']+(1-gini_df['p1_l'])*(1-gini_df['p1_l']))
+			gini_df['gini_r'] = 1-(gini_df['p1_r']*gini_df['p1_r']+(1-gini_df['p1_r'])*(1-gini_df['p1_r']))
+			gini_df['gini'] = gini_df['gini_l']*gini_df['lable_l']/total_cnt+gini_df['gini_r']*gini_df['lable_r']/total_cnt
+			min_gini = gini_df['gini'].min()
+			if min_gini < sp_gini:
+				sp_gini = min_gini
+				sp_value = gini_df['gini'].idxmin()
+				sp_index = index
+		left = np.where(dataset[:, sp_index] <= sp_value)
+		right = np.where(dataset[:, sp_index] > sp_value)
+		sp_groups = (dataset[left], dataset[right])
+		sp_lables = (lables[left], lables[right])
 		return {'index': sp_index, 'value': sp_value, 'groups': sp_groups, 'lables': sp_lables}
 
 	@staticmethod
@@ -50,7 +59,7 @@ class PUTREE():
 			return 0
 
 	# Create child splits for a node or make terminal
-	def split(self, node,  depth):
+	def split(self, node,  depth, feature_id):
 		left, right = node['groups']
 		left_l, right_l = node['lables']
 		del(node['groups'])
@@ -69,21 +78,23 @@ class PUTREE():
 		elif left_l.shape[0] == abs(np.sum(left_l)):
 			node['left'] = self.to_terminal(left_l)
 		else:
-			node['left'] = self.get_split(left, left_l)
-			self.split(node['left'], depth+1)
+			node['left'] = self.get_split(left, left_l, feature_id)
+			self.split(node['left'], depth+1, feature_id)
 		# process right child
 		if len(right) <= self.min_size:
 			node['right'] = self.to_terminal(right)
 		elif right_l.shape[0] == abs(np.sum(right_l)):
 			node['right'] = self.to_terminal(right_l)
 		else:
-			node['right'] = self.get_split(right, right_l)
-			self.split(node['right'], depth+1)
+			node['right'] = self.get_split(right, right_l, feature_id)
+			self.split(node['right'], depth+1, feature_id)
 
 	# Build a decision tree
-	def fit(self, train, lables):
-		root = self.get_split(train, lables)
-		self.split(root, 1)
+	def fit(self, train, lables, feature_id=None):
+		if feature_id == None:
+			feature_id = range(0, train.shape[1])
+		root = self.get_split(train, lables, feature_id)
+		self.split(root, 1, feature_id)
 		self.tree = root
 
 	# Print a decision tree
@@ -97,22 +108,24 @@ class PUTREE():
 		else:
 			print('%s[%s]' % ((depth*' ', node)))
 
-	def predict_x(self, node, row):
-		if row[node['index']] < node['value']:
-			if isinstance(node['left'], dict):
-				return self.predict_x(node['left'], row)
-			else:
-				return node['left']
+	def go_node(self, node, xs, indices):
+		left = np.where(xs[indices, node['index']] <= node['value'])[0]
+		if isinstance(node['left'], dict):
+			self.go_node(node['left'], xs, indices[left])
 		else:
-			if isinstance(node['right'], dict):
-				return self.predict_x(node['right'], row)
-			else:
-				return node['right']
+			self.y_pred[indices[left]] = node['left']
+		right = np.where(xs[indices, node['index']] > node['value'])[0]
+		if isinstance(node['right'], dict):
+			self.go_node(node['right'], xs, indices[right])
+		else:
+			self.y_pred[indices[right]] = node['right']
 
 	def predict(self, xs):
-		y = np.zeros(xs.shape[0])
-		for i in range(0, xs.shape[0]):
-			y[i] = self.predict_x(self.tree, xs[i, :])
+		self.y_pred = np.ones(xs.shape[0])*-1
+		indices = np.array(range(0, xs.shape[0]))
+		self.go_node(self.tree, xs, indices)
+		y = self.y_pred.copy()
+		del self.y_pred
 		return y
 
 	def etk(self, x_test, y_test):
@@ -125,30 +138,65 @@ class PUTREE():
 		return tk
 
 
-class PURF():
-	def __init__(self, max_depth, min_size, n_tree=10, boot_r=0.7):
+def parallel_call(params):  # a helper for calling 'remote' instances
+	cls = getattr(sys.modules[__name__], params[0])  # get our class type
+	instance = cls.__new__(cls)  # create a new instance without invoking __init__
+	instance.__dict__ = params[1]  # apply the passed state to the new instance
+	method = getattr(instance, params[2])  # get the requested method
+	args = params[3] if isinstance(params[3], (list, tuple)) else [params[3]]
+	return method(*args)  # expand arguments, call our method and return the result
+
+class PURF(object):
+	def __init__(self, max_depth, min_size, n_tree=10, boot_r=0.7, n_jobs=2, max_features=None, random_state=0):
 		self.n_tree = n_tree
 		self.forest = []
-		self.boot_r =boot_r
+		self.boot_r = boot_r
 		self.max_depth = max_depth
 		self.min_size = min_size
+		cpu_cnt = mp.cpu_count()
+		self.n_jobs = min(n_jobs, n_tree, cpu_cnt)
+		self.max_features = max_features
+		self.random_state = random_state
 
 	def fit(self, x_train, y_train):
+		# try multi_process
+		fit_args = []
 		for i in range(0, self.n_tree):
-			tree = self.get_trees(x_train, y_train)
-			self.forest.append(tree)
+			fit_args.append((x_train, y_train, i))
+		p = mp.Pool(processes=self.n_jobs)
+		result = p.map(parallel_call, self.prepare_call("get_trees", fit_args))
+		p.close()
+		# p.join()
+		print(result)
+		self.forest = result
+		# self.forest = [res.get() for res in result]
 
-	def bootstrap(self, y_train):
+		# # normal
+		# for i in range(0, self.n_tree):
+		# 	tree = self.get_trees(x_train, y_train, i)
+		# 	self.forest.append(tree)
+
+	def prepare_call(self, name, args):  # creates a 'remote call' package for each argument
+		for arg in args:
+			yield [self.__class__.__name__, self.__dict__, name, arg]
+
+	def bootstrap(self, x_train, y_train, random_state):
 		pos = np.where(y_train == 1)[0]
-		unls = np.where(y_train == -1)[0]
-		pos = np.random.shuffle(pos)
-		unls = np.random.shuffle(unls)
-		train = np.append(pos[:np.floor(self.boot_r*pos.__len__())], unls[:np.floor(self.boot_r*unl.__len__())])
-		val = np.append(pos[np.floor(self.boot_r*pos.__len__()):], unls[np.floor(self.boot_r*unl.__len__()):])
-		return (train, val)
+		unl = np.where(y_train == -1)[0]
+		np.random.seed(random_state)
+		np.random.shuffle(pos)
+		np.random.shuffle(unl)
+		train_id = np.append(pos[:np.int(self.boot_r*pos.__len__())], unl[:np.int(self.boot_r*unl.__len__())])
+		val_id = np.append(pos[np.int(self.boot_r*pos.__len__()):], unl[np.int(self.boot_r*unl.__len__()):])
 
-	def get_trees(self, x_train, y_train):
-		train_id, val_id = self.bootstrap(y_train)
+		feature_len = min(self.max_features, x_train.shape[1])
+		features = range(0, x_train.shape[1])
+		np.random.shuffle(features)
+		feature_id = features[: feature_len]
+		return train_id, val_id, feature_id
+
+	def get_trees(self, x_train, y_train, random_state):
+		train_id, val_id, feature_id = self.bootstrap(x_train, y_train, random_state)
 		train_x = x_train[train_id]
 		train_y = y_train[train_id]
 		val_x = x_train[val_id]
@@ -157,7 +205,7 @@ class PURF():
 		for j in range(1, 10):
 			pos_level = 1.0*j/10
 			pu_tree = PUTREE(pos_level, self.max_depth, self.min_size)
-			pu_tree.fit(train_x, train_y)
+			pu_tree.fit(train_x, train_y, feature_id)
 			etk = pu_tree.etk(val_x, val_y)
 			if etk < best_etk:
 				best_etk = etk
@@ -165,7 +213,15 @@ class PURF():
 		return final_tree
 
 	def predict(self, x_pred):
-		pass
+		y_preds = []
+		for tree in self.forest:
+			y_preds.append(tree.predict(x_pred))
+		y_preds_array = np.array(y_preds).transpose()
+		y_pred = (1.0*np.sum(y_preds_array, axis=1)/self.n_tree >= 0.5)
+		return y_pred
+
+
+
 
 
 
@@ -178,15 +234,26 @@ dataset = np.array([[2.771244718,1.784783929, -1],
 	[2.999208922,2.209014212,-1],
 	[7.497545867,3.162953546,1],
 	[9.00220326,3.339047188,1],
-	[7.444542326,0.476683375,1],
+	[7.444542326,0.476683375,-1],
 	[10.12493903,3.234550982,1],
 	[6.642287351,3.319983761,1]])
+#
+# pu_tree = PUTREE(0.9, 5, 1)
+# pu_tree.fit(dataset, dataset[:, -1])
+# pu_tree.print_tree()
+# y = pu_tree.predict(dataset)
+# etk = pu_tree.etk(dataset, dataset[:, -1])
+# print etk
+# print(y)
 
-pu_tree = PUTREE(0.5, 5, 1)
-pu_tree.fit(dataset, dataset[:, -1])
-pu_tree.print_tree()
-y = pu_tree.predict(dataset)
-etk = pu_tree.etk(dataset, dataset[:, -1])
-print etk
-print(y)
-
+# purf = PURF(max_depth=1e08, min_size=1, n_tree=10, boot_r=0.7, n_jobs=2, max_features=2, random_state=0)
+# purf.fit(dataset, dataset[:, -1])
+# y = purf.predict(dataset)
+# print(y)
+print(time.asctime(time.localtime(time.time())))
+if __name__ == "__main__":
+	purf = PURF(max_depth=1e08, min_size=1, n_tree=10, boot_r=0.7, n_jobs=3, max_features=2, random_state=0)
+	purf.fit(dataset, dataset[:, -1])
+	y = purf.predict(dataset)
+	print(y)
+print(time.asctime(time.localtime(time.time())))
